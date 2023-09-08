c16d909fcd7db6895a2bc3b2795f168fc1f584eb
darantnel <darantnel@gmail.com>
2023-09-08 00:10:48 -0400

    added new models for Eloquent tables

---
+++ app/Models/LessonUser.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class LessonUser extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'lesson_id',
+        'watched',
+
+    ];
+
+    public function user()
+    {
+        return $this->belongsToMany(User::class);
+    }
+}
---
+++ app/Models/UserBadge.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserBadge extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'badge_id',
+        'obtained',
+
+    ];
+
+    public function user()
+    {
+        return $this->belongsToMany(User::class);
+    }
+}
