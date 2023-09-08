2b374a6803bac2f4a3c9cf78d80f5c472ba58f2b
darantnel <darantnel@gmail.com>
2023-09-08 00:12:04 -0400

    add further logic to comment controller to retrieve achievements

--- app/Http/Controllers/CommentsContoller.php
+++ app/Http/Controllers/CommentsContoller.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 use App\Events\CommentWritten;
 use App\Models\Comment;
+use App\Models\User;
+use App\Models\Achievement;
+use App\Models\Badge;
 use Illuminate\Http\Request;
 
 class CommentsContoller extends Controller
@@ -13,16 +16,36 @@ class CommentsContoller extends Controller
             'id' => "",
             'user_id' => $request->user_id,
             'body' => $request->body,
-            'created_at' => $request->created_at,
-            'updated_at' => $request->updated_at,
+            'created_at' => date("Y-m-d h:i:s"),
+            'updated_at' => date("Y-m-d h:i:s"),
         ]);
        
         try {
-            CommentWritten::dispatch($csomment);
+            CommentWritten::dispatch($comment);
         } catch (Exception $e) {
             return "Failed to insert $comment";
         }
-        
+    
+        $user = User::find($request->user_id);
+        $number_of_comments_posted = $user->comments->count();
+
+        $records = Achievement::where('achievement_type_id', 2)->where('achievement_value', $number_of_comments_posted)->get();
+
+        return $records;
+        if ($lessonsWatched == 0) {
+            $user->unlockAchievement('First Lesson Watched');
+        } elseif ($lessonsWatched == 4) {
+            $user->unlockAchievement('5 Lessons Watched');
+        } elseif ($lessonsWatched == 9) {
+            $user->unlockAchievement('10 Lessons Watched');
+        } elseif ($lessonsWatched == 24) {
+            $user->unlockAchievement('25 Lessons Watched');
+        } elseif ($lessonsWatched == 49) {
+            $user->unlockAchievement('50 Lessons Watched');
+        }
+
+
+        return $commentsPosted;
         return $comment;
         
     }
