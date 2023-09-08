da0896d8c89b5fb0b4546a8ca2b7c9ab6481c113
darantnel <darantnel@gmail.com>
2023-09-07 21:49:35 -0400

    added new migrations for badge and achievement tables

--- app/Models/Achievement.php
+++ app/Models/Achievement.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
+use App\Models\Achievement;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Achievement extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name'
+    ];
 }
--- app/Models/Badge.php
+++ app/Models/Badge.php
@@ -2,10 +2,14 @@
 
 namespace App\Models;
 
+use App\Models\Badge;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Badge extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'name'
+    ];
 }
