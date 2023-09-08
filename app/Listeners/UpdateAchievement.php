aebd114209962bade075e1880c55b766fa466b4d
darantnel <darantnel@gmail.com>
2023-09-08 03:19:55 -0400

    added listener for achievement unlocked

--- app/Listeners/UpdateAchievement.php
+++ app/Listeners/UpdateAchievement.php
@@ -4,6 +4,8 @@ namespace App\Listeners;
 
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
+use App\Models\UserAchievement;
+use App\Models\Achievement;
 
 class UpdateAchievement
 {
@@ -20,6 +22,14 @@ class UpdateAchievement
      */
     public function handle(object $event): void
     {
-        //
+        $achievement_name = $event->achievement_name;
+        $user = $event->user;
+        $record = Achievement::where('name', $achievement_name)->get("id");
+        $achievement_id = $record[0]->id;
+        $user_achievement = new UserAchievement();
+        $user_achievement->user_id = $user->id;
+        $user_achievement->achievement_id = $achievement_id;
+        $user_achievement->obtained = 1;
+        $user_achievement->save();
     }
 }
