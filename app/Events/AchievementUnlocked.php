f708b3a33b70fdcd3206556f5b761f37a87aefd7
darantnel <darantnel@gmail.com>
2023-09-08 03:19:19 -0400

    added event for achievement unlocked

--- app/Events/AchievementUnlocked.php
+++ app/Events/AchievementUnlocked.php
@@ -9,28 +9,24 @@ use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
+use App\Models\UserAchievement;
+use App\Models\User;
 
 class AchievementUnlocked
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
-    /**
-     * Create a new event instance.
-     */
-    public function __construct()
-    {
-        //
-    }
+    public $achievement_name;
+    public $user;
 
     /**
-     * Get the channels the event should broadcast on.
+     * Create a new event instance.
      *
-     * @return array<int, \Illuminate\Broadcasting\Channel>
+     * @return void
      */
-    public function broadcastOn(): array
+    public function __construct($achievement_name, User $user)
     {
-        return [
-            new PrivateChannel('channel-name'),
-        ];
+        $this->achievement_name = $achievement_name;
+        $this->user = $user;
     }
 }
