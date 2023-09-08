4562a54ba4be4206cc9122ea63f51bfce315b055
darantnel <darantnel@gmail.com>
2023-09-08 03:20:56 -0400

    added listener for badge unlocked

--- app/Listeners/UpdateBadge.php
+++ app/Listeners/UpdateBadge.php
@@ -4,6 +4,8 @@ namespace App\Listeners;
 
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
+use App\Models\UserBadge;
+use App\Models\Badge;
 
 class UpdateBadge
 {
@@ -20,6 +22,14 @@ class UpdateBadge
      */
     public function handle(object $event): void
     {
-        //
+        $badge_name = $event->badge_name;
+        $user = $event->user;
+        $record = Badge::where('name', $badge_name)->get("id");
+        $badge_id = $record[0]->id;
+        $user_badge = new UserBadge();
+        $user_badge->user_id = $user->id;
+        $user_badge->achievement_id = $badge_id;
+        $user_badge->obtained = 1;
+        $user_badge->save();
     }
 }
