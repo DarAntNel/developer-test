889ae062727a0a4106aaefedaaa0a0115204962d
darantnel <darantnel@gmail.com>
2023-09-08 21:33:05 -0400

    finished logic for achievements endpoint by adding current badge, next badge and remaining to unlock next badge calculation functionality

--- app/Http/Controllers/AchievementsController.php
+++ app/Http/Controllers/AchievementsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Models\Achievement;
+use App\Models\Badge;
 use App\Models\AchievementType;
 
 class AchievementsController extends Controller
@@ -27,24 +28,46 @@ class AchievementsController extends Controller
                 $latest_achievements[$achievement_details[0]->achievement_type_id] = $achievement_details[0]->id;
             }
         }
-        
+    
         foreach($latest_achievements as $latest_achievement_type_id => $latest_achievement_id){
             if(!empty($latest_achievement_id)){
                 $latest_achievement_value = Achievement::where('id', $latest_achievement_id)->get("achievement_value")[0]->achievement_value;
                 $next_achievement_details = Achievement::where('achievement_type_id', $latest_achievement_type_id)->where('achievement_value', '>', $latest_achievement_value)->orderBy('achievement_value', 'asc')->take(1)->get();
-                $next_achievement_array[] = $next_achievement_details[0]->name;
+                ($next_achievement_details->isEmpty())? $next_achievement_array[] = "You have reached the maximum achievement": $next_achievement_array[] = $next_achievement_details[0]->name;
             }else{
                 $next_achievement_details = Achievement::where('achievement_type_id', $latest_achievement_type_id)->where('achievement_value', '>=', 0)->orderBy('achievement_value', 'asc')->take(1)->get();
-                $next_achievement_array[] = $next_achievement_details[0]->name;
+                ($next_achievement_details->isEmpty())? $next_achievement_array[] = "You have reached the maximum achievement": $next_achievement_array[] = $next_achievement_details[0]->name;
             }
         }
 
+        $badges = $user->badges;
+
+        foreach($badges as $badge){
+            $current_badge_value = 0;
+            $badge_details = Badge::where('id', $badge->badge_id)->get();
+            if ($badge_details[0]->badge_value >= $current_badge_value){
+                $current_badge = $badge_details[0]->name;
+                $current_badge_value = $badge_details[0]->badge_value;
+            }
+        }
+
+        $next_badge_details = Badge::where('badge_value','>', $current_badge_value)->orderBy('badge_value', 'asc')->take(1)->get();
+        if(!$next_badge_details->isEmpty()){
+            $next_badge = $next_badge_details[0]->name;
+            $number_of_achievements = $user->achievements->count();
+            $remaing_to_unlock_next_badge = $next_badge_details[0]->badge_value - $number_of_achievements;
+        }else{
+            $next_badge = "You have reached the maximum achievement";
+            $remaing_to_unlock_next_badge = 0;
+        }
+        
+       
         return response()->json([
             'unlocked_achievements' => $achievement_name_array,
             'next_available_achievements' => $next_achievement_array,
-            'current_badge' => '',
-            'next_badge' => '',
-            'remaing_to_unlock_next_badge' => 0
+            'current_badge' => $current_badge,
+            'next_badge' => $next_badge,
+            'remaing_to_unlock_next_badge' => $remaing_to_unlock_next_badge
         ]);
     }
 }
