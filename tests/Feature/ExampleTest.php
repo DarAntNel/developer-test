966679e87517fe595160bf6d8b915dcc0f692b5c
darantnel <darantnel@gmail.com>
2023-09-09 00:59:21 -0400

    added more test cases

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -243,4 +243,109 @@ class ExampleTest extends TestCase
         
     }
 
+    public function test_the_application_25_lessons_watched_and_10_comments_written(): void
+    {
+
+        $user = User::factory()->create();
+        
+        foreach (range(1, 25) as $x) {
+
+            $lesson = Lesson::find(rand(1, 20));   
+
+            $lesson_string = json_encode($lesson);
+            $lesson_array = json_decode($lesson_string, true);
+            $lesson_array["user_id"] = $user->id;
+
+            $response = $this->json('POST', '/api/watch_lesson', $lesson_array);
+        
+            $this->assertDatabaseHas('lesson_users', [
+                'user_id' => $user->id,
+                'lesson_id' => $lesson->id,
+            ]);
+        }
+
+        $comment = new Comment();
+        $comment->user_id = $user->id;
+        $comment->created_at = date("Y-m-d h:i:s");
+        $comment->updated_at = date("Y-m-d h:i:s");
+        $comment_string = json_encode($comment);
+        $comment_array = json_decode($comment_string, true);
+
+        foreach (range(1, 10) as $comment_number) {
+            
+            $comment_array["body"] = "Comment Body - $comment_number";
+
+            $response = $this->json('POST', '/api/post_comment', $comment_array);
+
+            $this->assertDatabaseHas('comments', [
+                'user_id' => $user->id,
+                'body' => $comment_array["body"],
+            ]);
+        }
+        
+        $response = $this->get("/users/{$user->id}/achievements");
+
+        $response->assertJson([
+            'unlocked_achievements' => ['First Lesson Watched', '5 Lessons Watched', '10 Lessons Watched', '25 Lessons Watched', 'First Comment Written', '3 Comments Written', '5 Comments Written', '10 Comments Written'],
+            'next_available_achievements' => ['50 Lessons Watched', '20 Comments Written'],
+            'current_badge' => 'Advanced',
+            'next_badge' => 'Master',
+            'remaing_to_unlock_next_badge' => 2,
+        ]);
+        
+    }
+
+    public function test_the_application_10_lessons_watched_and_10_comments_written(): void
+    {
+
+        $user = User::factory()->create();
+        
+        foreach (range(1, 10) as $x) {
+
+            $lesson = Lesson::find(rand(1, 20));   
+
+            $lesson_string = json_encode($lesson);
+            $lesson_array = json_decode($lesson_string, true);
+            $lesson_array["user_id"] = $user->id;
+
+            $response = $this->json('POST', '/api/watch_lesson', $lesson_array);
+        
+            $this->assertDatabaseHas('lesson_users', [
+                'user_id' => $user->id,
+                'lesson_id' => $lesson->id,
+            ]);
+        }
+
+        $comment = new Comment();
+        $comment->user_id = $user->id;
+        $comment->created_at = date("Y-m-d h:i:s");
+        $comment->updated_at = date("Y-m-d h:i:s");
+        $comment_string = json_encode($comment);
+        $comment_array = json_decode($comment_string, true);
+
+        foreach (range(1, 10) as $comment_number) {
+            
+            $comment_array["body"] = "Comment Body - $comment_number";
+
+            $response = $this->json('POST', '/api/post_comment', $comment_array);
+
+            $this->assertDatabaseHas('comments', [
+                'user_id' => $user->id,
+                'body' => $comment_array["body"],
+            ]);
+        }
+        
+        $response = $this->get("/users/{$user->id}/achievements");
+
+        $response->assertJson([
+            'unlocked_achievements' => ['First Lesson Watched', '5 Lessons Watched', '10 Lessons Watched', 'First Comment Written', '3 Comments Written', '5 Comments Written', '10 Comments Written'],
+            'next_available_achievements' => ['25 Lessons Watched', '20 Comments Written'],
+            'current_badge' => 'Intermediate',
+            'next_badge' => 'Advanced',
+            'remaing_to_unlock_next_badge' => 1,
+        ]);
+        
+    }
+
+
 }
